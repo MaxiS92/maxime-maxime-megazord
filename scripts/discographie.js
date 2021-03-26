35d95217a1de5d7c8271c6200b2b23e971bfde4a
M4xT1taN <max2002666@gmail.com>
2021-03-25 23:39:47 -0400

    scroll trigger sans animations boutons

--- scripts/discographie.js
+++ scripts/discographie.js
@@ -7,13 +7,39 @@ const swiper = new Swiper('.swiper-container', {
         }
   })
 
-  import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+  gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger);
+const sectionList = document.querySelectorAll(".ScrollTrigger, .disco-section");
+  sectionList.forEach((section) => { 
+  const photo= section.querySelector(".photo");
+  const ol = section.querySelector(".ol");
+  const video = section.querySelector(".video");
+  const card = section.querySelectorAll(".card");
+                    
+  gsap.timeline({
+    scrollTrigger: {
+    markers: false,
+    toggleActions: 'restart complete reverse reset',
+    trigger: section,
+    
+    },
+  })
 
-
-
-
-
-
+  .from(video, {
+    opacity: 0,
+    scale: '2',
+  })
+  .from(photo, {
+    y: '-100px',
+    opacity: 0,
+  })
+  .from(ol, {
+    y: '100px',
+    opacity: 0,
+  })
+  .from(card, {
+    y: '100px',
+    duration: 1,
+    opacity: 0,
+  })
+})
